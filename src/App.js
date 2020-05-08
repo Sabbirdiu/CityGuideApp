ce79302cabed211bf45aaa1b39aa56e1dbdd00e1
Sabbirdiu <sabbir15-8767@diu.edu.bd>
2020-05-08 23:14:15 +0600

    add the footer components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import Header  from './components/Header&Footer/Header'
+import Header from './components/Header&Footer/Header';
+import Footer from './components/Header&Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-     <Header/>
+    <div className='App'>
+      <Header />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Header&Footer/Footer.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div className='main-footer'>
+      <div className='container'>
+        <div className='row'>
+          {/* col 1 */}
+          <div className='col-md-3 col-sm-6'>
+            <h4>lorem ipsm</h4>
+            <ul className='list-unstyled'>
+              <li>lorem</li>
+              <li>lorem</li>
+              <li>lorem</li>
+              <li>lorem</li>
+            </ul>
+          </div>
+          {/* col 1 */}
+          <div className='col-md-3 col-sm-6'>
+            <h4>lorem ipsm</h4>
+            <ul className='list-unstyled'>
+              <li>lorem</li>
+              <li>lorem</li>
+              <li>lorem</li>
+              <li>lorem</li>
+            </ul>
+          </div>
+          {/* col 1 */}
+          <div className='col-md-3 col-sm-6'>
+            <h4>lorem ipsm</h4>
+            <ul className='list-unstyled'>
+              <li>lorem</li>
+              <li>lorem</li>
+              <li>lorem</li>
+              <li>lorem</li>
+            </ul>
+          </div>
+          {/* col 1 */}
+          <div className='col-md-3 col-sm-6'>
+            <h4>lorem ipsm</h4>
+            <ul className='list-unstyled'>
+              <li>lorem</li>
+              <li>lorem</li>
+              <li>lorem</li>
+              <li>lorem</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
