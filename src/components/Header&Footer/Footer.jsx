72964799adeec073f63e41ca47dd53501d5a14f2
Sabbirdiu <sabbir15-8767@diu.edu.bd>
2020-05-09 18:34:54 +0600

    update the style for footer

--- src/components/Header&Footer/Footer.jsx
+++ src/components/Header&Footer/Footer.jsx
@@ -1,54 +1,106 @@
 import React from 'react';
-
+import styled from 'styled-components';
 function Footer() {
   return (
-    <div className='main-footer'>
-      <div className='container'>
-        <div className='row'>
-          {/* col 1 */}
-          <div className='col-md-3 col-sm-6'>
-            <h4>lorem ipsm</h4>
-            <ul className='list-unstyled'>
-              <li>lorem</li>
-              <li>lorem</li>
-              <li>lorem</li>
-              <li>lorem</li>
-            </ul>
+    <FooterContainer className='main-footer'>
+      <div className='footer-middle'>
+        <div className='container'>
+          <div className='row'>
+            {/* col 1 */}
+            <div className='col-md-3 col-sm-6'>
+              <h4>lorem ipsm</h4>
+              <ul className='list-unstyled'>
+                <li>lorem</li>
+                <li>lorem</li>
+                <li>lorem</li>
+                <li>lorem</li>
+              </ul>
+            </div>
+            {/* col 1 */}
+            <div className='col-md-3 col-sm-6'>
+              <h4>lorem ipsm</h4>
+              <ul className='list-unstyled'>
+                <li>
+                  <a href='/'>lorem</a>
+                </li>
+                <li>
+                  <a href='/'>lorem</a>
+                </li>
+                <li>
+                  <a href='/'>lorem</a>
+                </li>
+                <li>
+                  <a href='/'>lorem</a>
+                </li>
+              </ul>
+            </div>
+            {/* col 1 */}
+            <div className='col-md-3 col-sm-6'>
+              <h4>lorem ipsm</h4>
+              <ul className='list-unstyled'>
+                <li>
+                  <a href='/'>lorem</a>
+                </li>
+                <li>
+                  <a href='/'>lorem</a>
+                </li>
+                <li>
+                  <a href='/'>lorem</a>
+                </li>
+                <li>
+                  <a href='/'>lorem</a>
+                </li>
+              </ul>
+            </div>
+            {/* col 1 */}
+            <div className='col-md-3 col-sm-6'>
+              <h4>lorem ipsm</h4>
+              <ul className='list-unstyled'>
+                <li>
+                  <a href='/'>lorem</a>
+                </li>
+                <li>
+                  <a href='/'>lorem</a>
+                </li>
+                <li>
+                  <a href='/'>lorem</a>
+                </li>
+                <li>
+                  <a href='/'>lorem</a>
+                </li>
+              </ul>
+            </div>
           </div>
-          {/* col 1 */}
-          <div className='col-md-3 col-sm-6'>
-            <h4>lorem ipsm</h4>
-            <ul className='list-unstyled'>
-              <li>lorem</li>
-              <li>lorem</li>
-              <li>lorem</li>
-              <li>lorem</li>
-            </ul>
-          </div>
-          {/* col 1 */}
-          <div className='col-md-3 col-sm-6'>
-            <h4>lorem ipsm</h4>
-            <ul className='list-unstyled'>
-              <li>lorem</li>
-              <li>lorem</li>
-              <li>lorem</li>
-              <li>lorem</li>
-            </ul>
-          </div>
-          {/* col 1 */}
-          <div className='col-md-3 col-sm-6'>
-            <h4>lorem ipsm</h4>
-            <ul className='list-unstyled'>
-              <li>lorem</li>
-              <li>lorem</li>
-              <li>lorem</li>
-              <li>lorem</li>
-            </ul>
+          <div className='footer-bottom'>
+            <p className='text-xs-center'>
+              &copy;{new Date().getFullYear()} CityGuide App - All Rights
+              Reserved
+            </p>
           </div>
         </div>
       </div>
-    </div>
+    </FooterContainer>
   );
 }
 
 export default Footer;
+
+const FooterContainer = styled.footer`
+  .footer-middle {
+    background: var(--mainDark);
+    padding-top: 3rem;
+    color: white;
+  }
+  .footer-bottom {
+    padding-top: 3rem;
+    padding-bottom: 2rem;
+  }
+
+  ul li a {
+    color: var(--mainGrey);
+    text-decoration: none;
+  }
+  ul li a:hover {
+    color: var(--mainLightGrey);
+  }
+`;
