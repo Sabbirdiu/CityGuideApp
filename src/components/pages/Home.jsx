b5f8a4b42b280a0413a5aab17dee8940b8940da8
Sabbirdiu <sabbir15-8767@diu.edu.bd>
2020-05-11 14:29:17 +0600

    update the style

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -7,13 +7,17 @@ import Info from '../Info';
 class Home extends Component {
   render() {
     return (
-      <InfoConsumer>
-        {(value) => {
-          return value.info.map((item) => {
-            return <Info key={item.id} item={item} />;
-          });
-        }}
-      </InfoConsumer>
+      <div className='container'>
+        <div className='row mt-5'>
+          <InfoConsumer>
+            {(value) => {
+              return value.info.map((item) => {
+                return <Info key={item.id} item={item} />;
+              });
+            }}
+          </InfoConsumer>
+        </div>
+      </div>
     );
   }
 }
