c12296dbea185b828e94a49fcb5eeaf4353dd874
Sabbirdiu <sabbir15-8767@diu.edu.bd>
2020-05-14 22:42:07 +0600

    update the map component

--- src/components/pages/Detail/Map.jsx
+++ src/components/pages/Detail/Map.jsx
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
-
+import {InfoConsumer} from '../../../Context'
 class Map extends Component {
   render() {
     return (
-      <div>
-        <h1>Map</h1>
-      </div>
+      <InfoConsumer>
+        {(data) => {
+          const { id, maps } = data.detailInfo;
+          return (
+            <div
+            id='map'
+            className='tab-pane in active text-center mt-5'
+            role='tabpanel'
+          >
+            <iframe
+              src={maps}
+              style={{
+                border: '0',
+                height: '28.12',
+                width: '100%',
+                frameborder: '0',
+              }}
+            ></iframe>
+          </div>
+          );
+        }}
+      </InfoConsumer>
     );
   }
 }
