ad742b0b95959cef36d9166901c64af9a0397816
sabbirdiu <sabbir15-8767@diu.edu.bd>
2020-08-23 11:31:25 +0600

    Updates

---
+++ precache-manifest.459e42eb37c4cde3cc4a65f622ce413d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b5a6dca8fa74341fc925ee47f53db69",
+    "url": "/TouristSpotGuide/index.html"
+  },
+  {
+    "revision": "8dc044d8a06dc2e16267",
+    "url": "/TouristSpotGuide/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "c0b15b63193597bc330a",
+    "url": "/TouristSpotGuide/static/css/main.b3b290ac.chunk.css"
+  },
+  {
+    "revision": "8dc044d8a06dc2e16267",
+    "url": "/TouristSpotGuide/static/js/2.1aa30998.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/TouristSpotGuide/static/js/2.1aa30998.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0b15b63193597bc330a",
+    "url": "/TouristSpotGuide/static/js/main.5f945672.chunk.js"
+  },
+  {
+    "revision": "70983c7833fe00b7ce0e",
+    "url": "/TouristSpotGuide/static/js/runtime-main.91a4771d.js"
+  }
+]);
